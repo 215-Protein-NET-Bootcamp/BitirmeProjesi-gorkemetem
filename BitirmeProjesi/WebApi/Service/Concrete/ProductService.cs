1cdfede166e6ff3938f3475ee480b3f3bfe12b89
Görkem Etem Irmak <gorkemetem98@gmail.com>
2022-08-16 22:48:57 +0300

    ValidationAspect added

--- BitirmeProjesi/WebApi/Service/Concrete/ProductService.cs
+++ BitirmeProjesi/WebApi/Service/Concrete/ProductService.cs
@@ -1,13 +1,36 @@
 ﻿using AutoMapper;
+using Base;
 using DataAccess;
 using Entities;
+using System;
+using System.Threading.Tasks;
 
 namespace Service
 {
     public class ProductService : BaseService<ProductDto, Product>, IProductService
     {
+        IProductRepository _repository;
         public ProductService(IProductRepository productRepository, IMapper mapper, IUnitOfWork unitOfWork) : base(productRepository, mapper, unitOfWork)
         {
+            _repository = productRepository;
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public override async Task<BaseResponse<ProductDto>> InsertAsync(ProductDto product)
+        {
+            try
+            {
+                var tempEntity = Mapper.Map<ProductDto, Product>(product);
+
+                await _repository.InsertAsync(tempEntity);
+                await UnitOfWork.CompleteAsync();
+
+                return new BaseResponse<ProductDto>(Mapper.Map<Product, ProductDto>(tempEntity));
+            }
+            catch (Exception ex)
+            {
+                throw new MessageResultException("Saving_Error", ex);
+            }
         }
     }
 }
---
+++ BitirmeProjesi/WebApi/Service/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using FluentValidation;
+
+namespace Service
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).MaximumLength(100).WithMessage("Product name must be maximum 100 characters");
+            RuleFor(p => p.ProductName).NotNull().WithMessage("Product name cannot be empty");
+            RuleFor(p => p.Description).MaximumLength(500).WithMessage("Description must be maximum 500 characters");
+            RuleFor(p => p.Description).NotNull().WithMessage("Description cannot be empty");
+            RuleFor(p => p.Price).GreaterThan(0).WithMessage("Price must be greter than 0.");
+            RuleFor(p => p.Price).NotNull().WithMessage("Price cannot be empty");
+        }
+    }
+}
